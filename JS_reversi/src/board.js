84a492f2aafd12b15faa329b57d7c7fb96dbaa74
Emmay <emmayalam1@gmail.com>
2021-09-29 13:55:24 -0400

    added board functions for board class

--- JS_reversi/src/board.js
+++ JS_reversi/src/board.js
@@ -9,6 +9,19 @@ if (typeof window === 'undefined'){
  * and two white pieces at [3, 3] and [4, 4]
  */
 function _makeGrid () {
+  const grid = [];
+  for (let i = 0; i < 8; i++) {
+    let row = new Array(8);
+    grid.push(row);
+  }
+
+
+  grid[3][4] = new Piece("black");
+  grid[4][3] = new Piece("black");
+  grid[3][3] = new Piece("white");
+  grid[4][4] = new Piece("white");
+
+  return grid;
 }
 
 /**
@@ -28,6 +41,13 @@ Board.DIRS = [
  * Checks if a given position is on the Board.
  */
 Board.prototype.isValidPos = function (pos) {
+  let x = pos[0];
+  let y = pos[1];
+
+  if (x < 0 || x > 7 || y < 0 || y > 7) {
+    return false;
+  };
+  return true;
 };
 
 /**
@@ -35,6 +55,14 @@ Board.prototype.isValidPos = function (pos) {
  * throwing an Error if the position is invalid.
  */
 Board.prototype.getPiece = function (pos) {
+  let x = pos[0];
+  let y = pos[1];
+
+  if (this.isValidPos(pos)) {
+    return this.grid[x][y];
+  } else {
+    throw new Error ("Not valid pos!");
+  }
 };
 
 /**
@@ -42,12 +70,27 @@ Board.prototype.getPiece = function (pos) {
  * matches a given color.
  */
 Board.prototype.isMine = function (pos, color) {
+  // let gotPiece = this.getPiece(pos)
+  if (this.isOccupied(pos) && this.getPiece(pos).color === color) {
+    return true;
+  } else {
+    return false;
+  };
 };
 
 /**
  * Checks if a given position has a piece on it.
  */
 Board.prototype.isOccupied = function (pos) {
+  let x = pos[0];
+  let y = pos[1];
+
+  let gotPiece = this.getPiece(pos);
+  if (gotPiece) {
+    return true;
+  } else {
+    return false;
+  };
 };
 
 /**
